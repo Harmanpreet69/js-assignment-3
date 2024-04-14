181624f03aad3d94d13d55e5002ba6830e47cf85
Harmanpreet69 <165360852+Harmanpreet69@users.noreply.github.com>
2024-04-14 02:18:50 -0400

    Got the js working

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,61 @@
+const locationForm = document.querySelector("#location-form");
+const locationSearch = document.querySelector("#location-form__search");
+const locationError = document.querySelector(".location-error");
+
+const city = document.querySelector(".weather-card__city");
+const state = document.querySelector(".weather-card__state");
+const temperature = document.querySelector(".weather-card__temperature");
+const description = document.querySelector(".weather-card__description");
+
 const apiKey = "ce00ef44568239e9f3d5333568586eb5";
+
+// Returns longitudes and latitudes of a city
+async function locationToCoordinates(queryCity) {
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${queryCity}&limit=1&appid=${apiKey}`;
+
+  try {
+    const res = await fetch(url);
+    const location = await res.json();
+
+    //   Update city and State on screen
+    city.textContent = location[0].name;
+    state.textContent = location[0].state;
+
+    return ({ lat, lon } = location[0]);
+  } catch (err) {
+    locationError.textContent =
+      "Location not found, Please enter a valid location!";
+  }
+}
+
+// Returns current weather of a city
+async function getWeatherData(queryCity) {
+  const { lat, lon } = await locationToCoordinates(queryCity);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  //   Make the fetch request to openweathermap.org
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  } catch (err) {}
+}
+
+locationForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  //   Resets error message
+  locationError.textContent = "";
+  try {
+    //   Destructures relevant weather info from the response
+    const {
+      main: { temp },
+      weather: [{ description: conditions }],
+    } = await getWeatherData(locationSearch.value);
+
+    //   Updates content on screen with the recived weather data
+    temperature.textContent = `${Math.round(temp)}°C`;
+    description.textContent = conditions;
+  } catch (err) {}
+});
